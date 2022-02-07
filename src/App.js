7bf6b25f36f28f4ca482a180331c7a8899a7ded5
Bomberman93 <nikita.bondarenko.1993@gmail.com>
2022-02-08 01:27:54 +0200

    finalgon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newContact = {
+      name: this.state.name,
+      id: nanoid(),
+    };
+    this.addToContacts(newContact);
+    // this.resetForm();
+  };
+
+  // resetForm = () => {
+  //   this.setState({ ...this.state });
+  //   console.log({ ...this.state });
+  // }
+
+  addToContacts = (newContact) => {
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+
+  render() {
+    const { contacts, name } = this.state;
+    const infoItem = contacts.map((contact) => {
+      return <li key={contact.id} className="">{contact.name}</li>;
+    });
+    return (
+      <div className="">
+        <div className="">
+          <h1 className="">Phonebook</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label className="">
+              Name
+              <input
+                type="text"
+                name="name"
+                value={name}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <h1>Contacts</h1>
+          <ul className="">
+            {infoItem}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
